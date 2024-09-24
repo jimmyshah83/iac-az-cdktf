007edcd7af475bfae05c133fa1b729c6387fb5f6
Jimmy Shah <jimmyshah83@gmail.com>
2024-09-24 14:31:08 -0400

    Working TF

--- aoai.tf
+++ aoai.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
-  name     = "js-eus-cdktf-aoai-01"
+  name     = "js-eus-cdktf-aoai-02"
   location = var.location
 }
 
@@ -14,10 +14,12 @@ resource "azurerm_cognitive_account" "example" {
   identity {
     type = "SystemAssigned" 
   }
+  public_network_access_enabled = false
+  custom_subdomain_name = var.custom_subdomain_name
 }
 
 resource "azurerm_key_vault" "example" {
-  name                     = "js-cdktf-example-vault"
+  name                     = "js-cdktf-example-kv"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
   tenant_id                = data.azurerm_client_config.current.tenant_id
@@ -60,7 +62,56 @@ resource "azurerm_cognitive_account_customer_managed_key" "example" {
   key_vault_key_id     = azurerm_key_vault_key.example.id
   timeouts {
     create = "500m"
-    update = "30m"
-    delete = "30m"
+    update = "500m"
+    delete = "500m"
   }
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/27"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_virtual_network.example.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+address_prefixes     = ["10.0.0.0/28"]
+}
+
+resource "azurerm_private_dns_zone" "example" {
+  name                = "privatelink.cognitiveservices.azure.com"
+  resource_group_name = azurerm_cognitive_account.example.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+  name                  = "example-link"
+  resource_group_name   = azurerm_cognitive_account.example.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.example.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+}
+
+resource "azurerm_private_endpoint" "example" {
+  name                = "example-private-endpoint"
+  location            = azurerm_cognitive_account.example.location
+  resource_group_name = azurerm_cognitive_account.example.resource_group_name
+  subnet_id           = azurerm_subnet.example.id
+
+  private_service_connection {
+    name                           = "example-privateserviceconnection"
+    private_connection_resource_id = azurerm_cognitive_account.example.id
+    subresource_names              = ["account"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name            = "example-dnszonegroup"
+    private_dns_zone_ids = [
+        azurerm_private_dns_zone.example.id
+    ]
+  }
+
+   depends_on = [ azurerm_private_dns_zone.example ]
 }
